e02ecf195b2a3bf0706ba71fe997b552ae741689
yindd <695685009@qq.com>
2019-09-18 20:59:27 +0800

    完成用户显示

--- src/main/java/com/ydd/controller/UserContorller.java
+++ src/main/java/com/ydd/controller/UserContorller.java
@@ -3,23 +3,29 @@ package com.ydd.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ydd.entity.User;
 import com.ydd.service.IUserService;
 
 @RequestMapping("/user")
+@Controller
 public class UserContorller {
 	
 	@Resource
 	private IUserService userService;
 	
-	@RequestMapping("/showUser")
-	public String showUserName(HttpServletRequest request, Model model){
-		int userId = Integer.parseInt(request.getParameter("id"));
-		User user = userService.queryOneUserById(userId);
-		model.addAttribute("user", user);
-		return "showUser";
+	@RequestMapping(value ="/getUserById")
+	@ResponseBody
+	public User getUserById(Integer id){
+		System.out.println("--------------------------");
+		User user = userService.queryOneUserById(id);
+		System.out.println(user);
+		//model.addAttribute("user", user);
+		//return "showUser";
+		return user ;
 	}
 }
